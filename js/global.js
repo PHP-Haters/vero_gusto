0180646a59b1ced526fb749259d22290bf35e521
Gabriel Castro <gabrielcastro.developer@gmail.com>
2024-09-04 20:11:05 -0300

    js for links on nav

--- js/global.js
+++ js/global.js
@@ -1,10 +1,7 @@
 const currentURL = window.location.href.split("/");
 const currentPage = currentURL[currentURL.length - 1];
-const firstPageLink = document.querySelector("#first-link");
-const secondPageLink = document.querySelector("#second-link");
-
-console.log(currentPage);
-console.log(firstPageLink);
+const firstPageLink = document.querySelector("#first-page");
+const secondPageLink = document.querySelector("#second-page");
 
 if (currentPage == "index.html") {
     firstPageLink.classList.add("current-page");
