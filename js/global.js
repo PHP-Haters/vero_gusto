6a6d53d2276e6c9e50736144e57a9a12dcc94d6d
Gabriel Castro <gabrielcastro.developer@gmail.com>
2024-09-05 10:51:56 -0300

    fixing css

--- js/global.js
+++ js/global.js
@@ -1,10 +1,16 @@
-const currentURL = window.location.href.split("/");
-const currentPage = currentURL[currentURL.length - 1];
 const firstPageLink = document.querySelector("#first-page");
 const secondPageLink = document.querySelector("#second-page");
 
-if (currentPage == "index.html") {
-    firstPageLink.classList.add("current-page");
-} else {
-    secondPageLink.classList.add("current-page");
-}
+window.addEventListener("load", () => {
+    const currentURL = window.location.href.split("/");
+    const currentPage = currentURL[currentURL.length - 1];
+    
+    console.log(currentPage);
+
+    if (currentPage == "index.html") {
+        firstPageLink.classList.add("current-page");
+    } else {
+        secondPageLink.classList.add("current-page");
+    }
+    
+})
