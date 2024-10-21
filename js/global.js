497e42758837670366acbc2c5bec75331636a77e
Guillermo del Toro Trillo <guillermotoro61@gmail.com>
2024-10-21 16:26:14 -0300

    Agora mostra alguma imagem

--- js/global.js
+++ js/global.js
@@ -5,6 +5,7 @@ document.querySelector(pageLink).classList.add("current-page");
 var inputQuantidadeSabores = document.getElementById("quantidade_sabores");
 var pizzaContainer = document.getElementById("container-pizza");
 
+var pedacosDePizza = [];
 function capturaQuantidadeSabores(){
     return parseFloat(inputQuantidadeSabores.value);
 }
@@ -15,11 +16,40 @@ function limpaQuantidadeSabores() {
     } 
 } 
 
+function umSabor(quantidade) {
+    for(var i = 0; i < quantidade; i++){
+        pedacosDePizza[i].innerHTML =  pedacosDePizza[i].innerHTML + "<img src='assets/images/montagem/pizza_um_sabor.png' alt=''>";
+    }
+}
+function doisSabores(quantidade) {
+    for(var i = 0; i < quantidade; i++){
+        console.log(pedacosDePizza[i]);
+        pedacosDePizza[i].innerHTML = "<img src='assets/images/montagem/pizza_um_sabor3.png' alt=''>";
+    }
+}
+function quatroSabores(quantidade) {
+    for(var i = 0; i < quantidade; i++){
+        console.log(pedacosDePizza[i]);
+        pedacosDePizza[i].innerHTML = "<img src='assets/images/montagem/pizza_um_sabor4.png' alt=''>";
+    }
+}
+
+
 inputQuantidadeSabores.addEventListener("change", () => {
+    
     let quantidade = capturaQuantidadeSabores();
     limpaQuantidadeSabores();
-
     for(var i = 0; i < quantidade; i++){
-        pizzaContainer.innerHTML = pizzaContainer.innerHTML + "<div class='pizza-slices'></div>";
+        pizzaContainer.innerHTML = pizzaContainer.innerHTML + "<div id='"+i+"Pedaco' class='pizza-slices'></div>";
+        pedacosDePizza[i] = document.getElementById(i+"Pedaco");
+    }
+    if(quantidade == 2) {
+        doisSabores(quantidade);
+    } 
+    else if(quantidade == 4) {
+        quatroSabores(quantidade);
+    }
+    else {
+        umSabor(quantidade);
     }
 });
