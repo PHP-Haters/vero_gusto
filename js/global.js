746e14056fa6373a4a9162ab80c4e5ffe7ea479c
Ana Ripoli <anaripoli.eng@gmail.com>
2024-11-05 20:43:07 -0300

    validação de dados

--- js/global.js
+++ js/global.js
@@ -31,3 +31,42 @@ inputQuantidadeSabores.onchange = function() {
         pizzaContainer.innerHTML = pizzaContainer.innerHTML + "<div class='pizza-slices '><img heigth=150 width=150 src='assets/images/montagem/pizza_um_sabor4.png' alt=''></div><div class='pizza-slices flipH'><img heigth=150 width=150 src='assets/images/montagem/pizza_um_sabor4.png' alt=''></div><div class='pizza-slices flipH flipV'><img heigth=150 width=150 src='assets/images/montagem/pizza_um_sabor4.png' alt=''></div><div class='pizza-slices'><img heigth=150 width=150 src='assets/images/montagem/pizza_imagem_4_invertida.png' alt=''></div>";
     }
 }
+
+
+const tamanhoPizza = document.querySelector("#tamanho");
+const quantidadeSabores = document.querySelector("#quantidade_sabores");
+const bordaPizza = document.querySelector("#borda");
+const saborBorda = document.querySelector("#saborBorda");
+const cep = document.querySelector("#cepInput");
+const rua = document.querySelector("#ruaInput");
+const telefone = document.querySelector("#telefoneInput");
+const bairro = document.querySelector("#bairroInput");
+const complemento = document.querySelector("#complementoInput");
+const usuario = document.querySelector("#nomeUsuarioInput");
+
+const formBtn = document.querySelector("#formBtn");
+
+formBtn.addEventListener("click", validarDados);
+
+function validarDados(event){
+    event.preventDefault();
+    
+    if (tamanhoPizza.value ||
+        quantidadeSabores.value ||
+        bordaPizza.value ||
+        saborBorda.value ||
+        cep.value ||
+        rua.value ||
+        telefone.value ||
+        bairro.value ||
+        complemento.value ||
+        usuario.value) {
+        mostrarPopUp();
+    } else {
+        window.alert("Ainda há campos vazios no formulário");
+    }
+}
+
+function mostrarPopUp(){
+    window.alert("Finge que isso aqui deu certo");
+}
